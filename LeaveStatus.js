70dd2621c6e0b3bb3ec79b6bc0bae383f4843544
Harsh Bansal <64414335+harshBansal47@users.noreply.github.com>
2024-03-20 10:04:25 +0530

    Update LeaveStatus.js

--- LeaveStatus.js
+++ LeaveStatus.js
@@ -1,37 +1,30 @@
 import React, { useEffect, useState } from "react";
-import Loader from "../Common/Loader";
+import LeaveForm from "./LeaveForm";
 import { Axios } from "../../config";
-import EmployeeStatusCard from "../TaskByDay/EmployeeStatusCard";
-import NoDataFound from "../Common/NoDataFound";
 import LeaveStatusCard from "./LeaveStatusCard";
-import axios from "axios";
-import LeaveForm from "../Common/LeaveForm";
+import Loader from "../Common/Loader";
 
-function LeaveStatus() {
-  const [isDialogBoxOpen, setDialogOpen] = useState(false);
+const LeaveStatus = () => {
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(false);
   const [leavesList, setLeavesList] = useState([]);
   const [baseUrl, setBaseUrl] = useState("");
-  const [filterLeaveList, setFilterLeaveList] = useState([]);
-  const [comingList, setComingList] = useState([]);
-  const [filterState, setFilterState] = useState(0);
-  const [empolyees, setEmpolyees] = useState([]);
+
 
   const openDialog = () => {
-    console.log("dialog box"+isDialogBoxOpen)
     setDialogOpen(true);
   };
 
-
-
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
 
   useEffect(() => {
     setLoading(true);
     Axios.get("leave-statuses")
       .then((res) => {
         setLeavesList(res.data.Data);
-        setFilterLeaveList(res.data.Data);
-        setComingList(res.data.Data);
       })
       .catch(() => {
         console.error("Errro when Fetching Room Details");
@@ -39,113 +32,102 @@ function LeaveStatus() {
       .finally(() => {
         setLoading(false);
       });
-      Axios.get('employees')
-      .then((res) => {
-          setEmpolyees(res.data.Data);
-      });
+    Axios.get("employees").then((res) => {
+      setEmployees(res.data.Data);
+    });
     const baseUrl = window.location.protocol + "//" + window.location.host;
     setBaseUrl(baseUrl);
-
-    return () => {};
-  }, [false]);
+  }, []);
 
   const handleClick = (e) => {
-    const value = e.target.value;
-    if (value === "1") {
-      setFilterState(1);
-      const comingList = leavesList.filter((leave) => {
-        return leave.timePeriod !== "full" || (leave.timePeriod === "half" && leave.noOfDays === 0);
-      });
-      setComingList(comingList); // Corrected assignment using setComingList
-    } else {
-      setFilterState(2);
-      const filterLeaveList = leavesList.filter((leave) => {
-        const leaveDate = new Date(leave.createdAt);
+    if (e === 1) {
+      // Handle click for "Coming Today" button
+      setLeavesList(leavesList.filter((leave) => {
+        const leaveDateStr = leave.leaveDate;
+        const leaveDateParts = leaveDateStr.split('-');
+        const leaveDate = new Date(`${leaveDateParts[2]}-${leaveDateParts[0]}-${leaveDateParts[1]}`);
+        
+        // Parse leave duration to integer
+        const leaveDuration = parseInt(leave.leaveDuration);
+    
+        // Calculate end date of the leave by adding leave duration to leave date
+        const arrivalDate = new Date(leaveDate.getTime() + (leaveDuration * 24 * 60 * 60 * 1000));
+    
+        // Get today's date
         const today = new Date();
-        return leaveDate.toDateString() === today.toDateString();
-      });
-      setFilterLeaveList(filterLeaveList); // Corrected assignment using setFilterLeaveList
+    
+        // Compare the end date of the leave with today's date
+        return arrivalDate.getTime() === today.getTime();
+    }));
+    
+
+    } else if (e === 2) {
+      // Handle click for "Leave from Today" button
+      setLeavesList(leavesList.filter((leave)=>{
+        const leaveDatestr = leave.leaveDate;
+        const leaveDateParts = leaveDatestr.split('-');
+        const leaveDate = new Date(`${leaveDateParts[2]}-${leaveDateParts[0]}-${leaveDateParts[1]}`);
+        const today = new Date();
+        return today.getTime() === leaveDate.getTime();
+      }))
     }
   };
-  
+
   return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <section className="availability">
-          <div className="flex justify-between items-center">
-          {isDialogBoxOpen && <LeaveForm isModalOpen={isDialogBoxOpen} data={{empolyees:empolyees}}/>}
-            <h2>Leave Status</h2>
-            <button
-              disabled={false}
-              className="booknow-btn smallButton"
-              onClick={openDialog} // Open dialog box on button click
-            >
-              Apply
-            </button>
-          </div>
-          {/* Filter Buttons */}
-          <div className="status-wrapper-filter">
-            <div className="task-status">
-              <button
-                className="outlined-button"
-                value={1}
-                onClick={handleClick}
-              >
-                Coming Today
-              </button>
-            </div>
-            <div className="task-assignee">
-              <button
-                className="outlined-button"
-                value={2}
-                onClick={handleClick}
-              >
-                Leave from Today
-              </button>
-            </div>
-          </div>
-          {/* Leave cards */}
-          <div className="room-card-wrapper">
-            {filterState === 0 ? (
-              leavesList.map((leave, index) => {
-                return (
-                  <LeaveStatusCard
-                    key={index}
-                    object={leave}
-                    handleClick={handleClick}
-                  />
-                );
-              })
-            ) : filterState === 1 ? (
-              comingList.map((leave, index) => {
-                return (
-                  <LeaveStatusCard
-                    key={index}
-                    object={leave}
-                    handleClick={handleClick}
-                  />
-                );
-              })
-            ) : filterState === 2 ? (
-              filterLeaveList.map((leave, index) => {
-                return (
-                  <LeaveStatusCard
-                    key={index}
-                    object={leave}
-                    handleClick={handleClick}
-                  />
-                );
-              })
-            ) : (
-              <NoDataFound />
-            )}
-          </div>
-        </section>
+   <>
+   {loading?<Loader/>:( <section className="availability">
+        {isDialogOpen && (
+        <LeaveForm
+          isModalOpen={isDialogOpen}
+          isModalClose={closeDialog}
+          data={{ employees: employees }}
+        />
       )}
-    </>
+      <div className="flex justify-between items-center">
+        <h2>Leave Status</h2>
+        <button
+          disabled={false}
+          className="booknow-btn smallButton"
+          onClick={openDialog} // Open dialog box on button click
+        >
+          Apply for leave
+        </button>
+      </div>
+
+      <div className="status-wrapper-filter">
+        <div className="task-status flex items-center ">
+          <button
+            className="outlined-button"
+            value={1}
+            onClick={() => handleClick(1)} // Pass a function reference to onClick
+          >
+            Coming Today
+          </button>
+        </div>
+        <div className="task-status">
+          <button
+            className="outlined-button"
+            value={2}
+            onClick={() => handleClick(2)} // Pass a function reference to onClick
+          >
+            Leave from Today
+          </button>
+        </div>
+      </div>
+      {/* Leave cards */}
+      <div className="room-card-wrapper">
+        { leavesList.map((leave, index) => {
+                return (
+                  <LeaveStatusCard
+                    key={index}
+                    object={leave}
+                    handleClick={handleClick}
+                  />
+                );})}
+      </div>
+    </section>)}
+   </>
   );
-}
+};
 
 export default LeaveStatus;
